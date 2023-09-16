ff489d2794228fc5a983f97223a1db25a22637c3
Hemanth B <hemanth22hemu@gmail.com>
2023-09-16 22:17:49 +0530

    Update reuters.py

--- reuters.py
+++ reuters.py
@@ -1,15 +1,10 @@
 import requests
 import json
 
-base_url = "https://www.reuters.com/markets/quote/USDEUR=X/"
+# URL of the Reuters page with the data
+url = "https://www.reuters.com/markets/quote/USDEUR=X/"
 
-payload = {}
-Healders = {}
+# Send an HTTP GET request to the URL
+response = requests.get(url)
 
-response = requests.request("GET", base_url)
-if response.status_code == 200:
-    rates_data = response.json()
-    print(rates_data)
-else:
-    print(f"Request failed with status code {response.status_code}")
-
+data = response.content.decode('utf-8')
